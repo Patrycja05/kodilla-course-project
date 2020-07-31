cbe96d7fee77db57c6d39e736ea705a2e691f855
Patrycja05 <patrycja.calka@wp.pl>
2020-07-31 22:10:07 +0200

    Zadanie 22.3

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,12 +1,17 @@
 package com.crud.tasks;
 
+import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.reflect.Method;
 
 @SpringBootApplication
 public class TasksApplication  {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TasksApplication.class, args);
+		Method method = TrelloClient.class.getDeclaredMethod("getTrelloBoards");
+		method.setAccessible(true);
 	}
 }
